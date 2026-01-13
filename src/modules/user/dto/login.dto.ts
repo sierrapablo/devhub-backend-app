82803ed1bd6ff49759c40f7c734b069e79f8bf68
Pablo Sierra Lorente <pablosierra@flit2go.com>
2026-01-13 16:26:21 +0100

    chore: add TODO comment

--- src/modules/user/dto/login.dto.ts
+++ src/modules/user/dto/login.dto.ts
@@ -5,6 +5,6 @@ export class LoginDto {
   email!: string;
 
   @IsString()
-  @MinLength(1)
+  @MinLength(1) // TODO: En un futuro, cuando no esté en pruebas, se modificará a un valor mínimo más alto por seguridad
   password!: string;
 }
