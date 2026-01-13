08f52a9af82eeae9b5c18c752af58babdcaedf5a
Pablo Sierra Lorente <pablosierra@flit2go.com>
2026-01-13 12:49:06 +0100

    code review: add cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prefix = process.env.API_PREFIX ? String(process.env.API_PREFIX) : '';
   if (prefix) app.setGlobalPrefix(prefix.replace(/^\//, ''));
+
+  app.enableCors();
+  app.use(helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
