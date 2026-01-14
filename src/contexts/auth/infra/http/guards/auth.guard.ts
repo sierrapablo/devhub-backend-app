2162b2f27cd4e787ba3d70dfcc30c4718cf5f6e5
Pablo Sierra Lorente <pablosierralorente@gmail.com>
2026-01-15 00:42:19 +0100

    fix: add correct import types

--- src/contexts/auth/infra/http/guards/auth.guard.ts
+++ src/contexts/auth/infra/http/guards/auth.guard.ts
@@ -1,4 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import type { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Inject } from '@nestjs/common';
 import type { Request } from 'express';
 import type { TokenService } from '#src/contexts/auth/application/ports/token-service.js';
